4b711f3d6ad4f26023f494faf8752259600faa21
Oleg Tihonenko <tihonenko.oleg@gmail.com>
2024-04-08 22:57:30 +0300

    oleg

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <code>hello, this is react tg- mini app</code>
         </p>
         <a
           className="App-link"
